529ceafed287092df74b170e3f4b6ddbdd7b77be
Дмитрий Сединкин <seadinkin2002@yandex.ru>
2023-02-26 18:13:54 +0500

    0.0.1

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import { ShortVideo } from "node-vk-clip-api";
 
-const TOKEN = "vk1.a.NHUvZGqoOSVBsDwxyD59xr3WuIjRd-Q3n5193RDFVvXZyahhUZah6pPjKvH_bCJ52gowKg8xkaf5Ot8ukAw8WEHKsnME5wpSUS0nSwfSkY9EPxR2SCaphjKwKCAj5PKzmcDUu6Z5YDPwemafGKeCrYMjiM1RT3bo-Dwa7e-TGW5Ls_mI9BKzxmIPTJP5HrXY"; // access_token.
+const TOKEN = ""; // access_token.
 
 const clip = new ShortVideo(TOKEN);
 
 const url = "./clip.mp4";
 
 // Загрузка клипа.
-clip.create(url, { group_id: 218880516, description: "Описание клипа #хештег1 #хештег2", wallpost: 1 })
+clip.create(url, { group_id: 1, description: "Описание клипа #хештег1 #хештег2", wallpost: 1 })
   .then((data) => {
     console.log({ data });
   })
